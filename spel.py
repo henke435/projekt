6c8000dad93a7f7fd41dc7175d08946c87d138cf
henke435 <henrik.lindberg@skola.taby.se>
2023-12-02 01:18:00 +0100

    ds

--- spel.py
+++ spel.py
@@ -1,5 +1,4 @@
 import keyboard
-import msvcrt
 import random as r
 import math
 import time
@@ -193,12 +192,12 @@ def monsterFight(chosenClass, lvl, gold, currentWeapon):
     
     addedGold = r.randint(50, 100)
     time.sleep(2)
-    if chosenClass.hp < 0:
+    if chosenClass.hp <= 0:
         print(f"\n        You get brutally killed by the {monster.name} and the lose the game")
         time.sleep(1.5)
         input("\nPress 'Enter' to return to the beginning!")
         return chosenClass, lvl, gold, False
-    elif monster.hp < 0:
+    elif monster.hp <= 0:
         print(f"\n        You slay the {monster.name} and gain 1 lvl and collect {addedGold} gold from its corpse")
         print(f"\n        You now have {chosenClass.hp} hp left")
         time.sleep(1.5)
@@ -210,7 +209,7 @@ def openChest(chosenClass):
     bruiserWeapons  = ["Steel Axe", "Broad Axe", "Swift Axe", "Double-edged Axe", "Enchanted Double-edged Axe"]
     archerWeapons  = ["Wooden Bow", "Black Bow", "Light Bow", "Swift Bow", "Enchanted Silver Bow"]
     assassinWeapons  = ["Steel Dagger", "Heavy Sword", "Longsword", "Magic Dagger", "Flaming Dagger"]
-    weaponStrength = [1.2, 1.4, 1.6, 1.8, 2]
+    weaponStrength = [1.4, 1.8, 2.2, 2.5, 3]
     amount = "a"
     if chosenClass.className == "Bruiser":
         chestList = bruiserWeapons
@@ -240,7 +239,7 @@ def openChest(chosenClass):
     else:
         return recievedItem, Item(recievedItem, weaponStrength[index])
 
-def merchant(gold, chosenClass):
+def merchant(gold, chosenClass, weapons, potions):
     bruiserWeapons  = ["Steel Axe", "Broad Axe", "Swift Axe", "Double-edged Axe", "Enchanted Double-edged Axe"]
     archerWeapons  = ["Wooden Bow", "Black Bow", "Light Bow", "Swift Bow", "Enchanted Silver Bow"]
     assassinWeapons  = ["Steel Dagger", "Heavy Sword", "Longsword", "Magic Dagger", "Flaming Dagger"]
@@ -271,11 +270,12 @@ def merchant(gold, chosenClass):
     answer = input("""
           
         Hello there traveler! Would you like a gander at my little shop?
-                    1. Yes                          2. No
+                    1. Yes                           2. No
+                               3. Sell something
                    
         """)
     #fixa bidding
-    while answer != "1" and answer != "2":
+    while answer != "1" and answer != "2" and answer != "3":
         print("\nPlease enter one of the available options!")
         answer = input("\n\n       ")
     if answer == "1":
@@ -290,7 +290,7 @@ def merchant(gold, chosenClass):
                 
         print(f"        {i + 2}. Health Potion")
         time.sleep(1)
-        return bidding(gold, merchantInventory)
+        return bidding(gold, merchantInventory, weapons, potions)
     elif answer == "2":
         print("""
 
@@ -298,9 +298,69 @@ def merchant(gold, chosenClass):
                   
 """)
         input("Press 'Enter' to continue!")   
-        return "", gold     
+        return "", weapons, gold
+    else:
+        weapons, gold = selling(gold, chosenClass, weapons)
+        return "", weapons, gold
+    
+def selling(gold, chosenClass, weapons):
+    keepSelling = True
+    if len(weapons) > 1:
+        while keepSelling:
+            print("""
 
-def bidding(gold, merchantInventory):
+            Alrighty, what would you like to sell?
+
+    """)
+            answerIsOk = False
+            for i in range (len(weapons)):
+                print(f"        {i + 1}. {weapons[i].name}  ({weapons[i].strength}/3)")
+                time.sleep(1)
+            while answerIsOk == False:
+                weaponToSell = input("\n        ")
+                try:
+                    x = int(weaponToSell)
+                except ValueError:
+                    print("\nPlease enter one of the available options!")
+                else:
+                    if int(weaponToSell) > 0 and int(weaponToSell) <= i + 1:
+                        answerIsOk = True
+            print("\n        Hmmm....")
+            time.sleep(2)
+            merchantBid = round(r.randint(40, 80)*weapons[int(weaponToSell) - 1].strength)
+            print(f"""\n        I think I can give you about {merchantBid} gold for that {weapons[int(weaponToSell) - 1].name}, whaddaya think?
+              1. Yes that will be great.     2. No, sorry, that's too little.
+                           3. I would rather sell something else.
+    """)                                            
+            answer = input("\n       ")
+            while answer != "1" and answer != "2" and answer != "3":
+                print("\nPlease enter one of the available options!")
+                answer = input("\n\n       ")
+            if answer == "1":
+                weapons.pop(int(weaponToSell)-1)
+                gold += merchantBid
+                if len(weapons) > 1:
+                    print("""\n       Pleasure doing business with you, would you like to sell something else?
+                                    1. Yes                   2. No
+    """)
+                    answer_2 = input("\n       ")
+                    while answer_2 != "1" and answer_2 != "2":
+                        print("\nPlease enter one of the available options!")
+                        answer_2 = input("\n\n       ")
+                    if answer_2 == "1":
+                        pass
+                    if answer_2 == "2":
+                        keepSelling = False
+                else: 
+                    keepSelling = False
+            elif answer == "2":
+                keepSelling = False
+                print("\n       Good luck on the path!")
+    else:
+        print("\n        Sorry but you don't seem to have enough items to sell")
+    return weapons, gold
+        
+def bidding(gold, merchantInventory, weapons, potions):
     answerIsOk = False
     frustration = 0
     merchantItem = ""
@@ -348,7 +408,7 @@ def bidding(gold, merchantInventory):
             
         """)
             input("Press 'Enter' to continue!") 
-            return "", gold
+            return "", weapons, gold
         else:
             print("\n        That's too low. Try a higher bid!\n\n")
             print(f"        Frustration: {round(frustration)}/100")
@@ -369,7 +429,7 @@ def bidding(gold, merchantInventory):
 
 """)    
         input("Press 'Enter' to continue!") 
-        return "", gold
+        return "", weapons, gold
     elif frustration < 100:
         print("""
 
@@ -379,36 +439,122 @@ def bidding(gold, merchantInventory):
         input("Press 'Enter' to continue!")  
         gold = gold - bid
         if merchantItem == "Health Potion":
-            return merchantItem, gold
+            return merchantItem, weapons, gold
         else:
-            return merchantInventory[int(answer)-1], gold
+            weapons.append(Item(merchantInventory[int(answer)-1].name, merchantInventory[int(answer)-1].strength))
+            return merchantInventory[int(answer)-1], weapons, gold
     
-def fallInTrap(chosenClass, alive):
+def fallInTrap(chosenClass, alive, gold, maxHp):
     alfabet = "abcdefghijklmnopqrstuvwxyz"
-    print("\n       ***You see a chest up ahead in the distance and start approaching it.***")
-    time.sleep(2)
-    print("\n       ***A few meters from it, you feel the planks under your feet start to fall***")
-    time.sleep(2)
-    print("\n       ***You try to hold yourself to the wall***")
-    time.sleep(1)
-    print("\n       ***Quickly press the prompted key to save yourself***")
-    time.sleep(3)
+    textVariation = r.randint(0,3)
+    addedGold = 0
+    if textVariation == 0:
+        print("\n       ***You see a chest up ahead in the distance and start approaching it.***")
+        time.sleep(2)
+        print("\n       ***A few meters from it, you feel the planks under your feet start to fall***")
+        time.sleep(2)
+        print("\n       ***You try to hold yourself to the wall***")
+        time.sleep(2)
+        print("\n       ***Quickly press the prompted key to save yourself***")
+        time.sleep(3)
+    elif textVariation == 1:
+        print("\n       ***You come across a mystical orb shimmering with magical energy. As you approach, the ground begins to shake.***")
+        time.sleep(2)
+        print("\n       ***A magical forcefield surrounds you, and you realize that the only way to unlock it is by solving a code puzzle.***")
+        time.sleep(2)
+        print("\n       ***Symbols and glyphs float in the air, forming a complex code.***")
+        time.sleep(2)
+        print("\n       ***Quickly press the prompted key to decipher the magical code and unlock the forcefield, or face the consequences.***")
+        time.sleep(3)
+    elif textVariation == 2:
+        print("\n       ***You stumble upon an ancient library hidden deep within a mysterious forest.***")
+        time.sleep(2)
+        print("\n       ***As you explore, the library's guardian, an ancient elf, locks you in and challenges you with a riddle.***")
+        time.sleep(2)
+        print("\n       ***The words on its pages rearrange themselves into a cryptic code.***")
+        time.sleep(2)
+        print("\n       ***Quickly press the prompted key to solve the riddle to regain your freedom.***")
+        time.sleep(3)
+    elif textVariation == 3:
+        print("\n       ***You find yourself in an abandoned laboratory filled with strange contraptions and flickering lights.***")
+        time.sleep(2)
+        print("\n       ***A holographic message materializes in front of you, presenting a complex mathematical problem.***")
+        time.sleep(2)
+        print("\n       ***Equations and symbols float in the air, forming a code that is the key to your freedom.***")
+        time.sleep(2)
+        print("\n       ***Quickly press the prompted key to solve the mathematical code and regain your freedom.***")
+        time.sleep(3)
     letter = alfabet[r.randint(0,len(alfabet) - 1)]
     print(f"\n       ***Press '{letter}' in under {chosenClass.trapRisk} seconds***")
     startTime = time.perf_counter()
     keyboard.wait(letter)
     endTime = time.perf_counter()
     if endTime - startTime < chosenClass.trapRisk:
+        solvedPuzzle = True
+        if textVariation == 0:
+            print("\n       ***You successfully grab onto the wall and climb out.***")
+            time.sleep(2)
+            print("\n       ***You escape the trap and leave without any harm.***")
+        elif textVariation == 1:
+            print("\n       ***The symbols align perfectly, and the forcefield dissipates. The forcefield opens before you.***")
+            time.sleep(2)
+            print("\n       ***You step out of the forcefield, stepping without harm.***")
+
+        elif textVariation == 2:
+            print("\n       ***The pages of the book settle, and the guardian nods approvingly.***")
+            time.sleep(2)
+            print("\n       ***The library opens again, and you leave without the guardian hurting you***")
+        elif textVariation == 3:
+            print("\n       ***The equations align perfectly, and the holographic message dissolves. The laboratory's doors open.***")
+            time.sleep(2)
+            print("\n       ***You walk out unscathed.***")
         
     else:
-        print("du torska")
-    return chosenClass, alive
+        solvedPuzzle = False
+        if textVariation == 0:
+            print("\n       ***You try to grab ahold of the wall, but your hand slips and you fall down a small cliff***")
+            time.sleep(2)
+            print("\n       ***You take some damage which leaves you halting out.***")
+        elif textVariation == 1:
+            print("\n       ***The symbols blur together, and the forcefield reacts violently.***")
+            time.sleep(2)
+            print("\n       ***A magical shockwave throws you back, and the orb sounds with an echoing hum.***")
+            time.sleep(2)
+            print("\n       ***Unable to withstand the magical backlash, you take damage due to the mystical forces.***")
+        elif textVariation == 2:
+            print("\n       ***The words on the pages become chaotic, and the guardian's eyes flash ominously.***")
+            time.sleep(2)
+            print("\n       ***A magical barrier seals the library's entrance, preventing you from accessing its knowledge.***")
+            time.sleep(2)
+            print("\n       ***Overwhelmed by the magical enchantments, you feel the guardian draining some of your health with magic.***")
+        elif textVariation == 3:
+            print("\n       ***The holographic message flickers, and an alarm echoes through the laboratory.***")
+            time.sleep(2)
+            print("\n       ***Security systems activate, releasing a swarm of nanobots that inflict damage. You stagger out of the laboratory wounded.***")
+    time.sleep(2)
+    if solvedPuzzle == True:
+        addedGold = r.randint(60, 120)
+        print(f"\n       ***After the close call, you stumble upon a small treasure of {addedGold} gold, and continue forward.***")
+        time.sleep(2)
+    if solvedPuzzle == False: 
+        chosenClass.hp -= r.randint(int(maxHp/3), maxHp)
+        if chosenClass.hp <= 0:
+            print("\n       ***You stumble away from the situation with great injuries.***")
+            time.sleep(2)
+            print("\n       ***Sitting down by a cliff-side, you watch upon the sunset one last time before taking your last breath.")
+            time.sleep(2)
+            alive = False
+        else:
+            print("\n       ***You stumble away from the situation with great injuries, barely surviving.***")
+            time.sleep(2)
+        print()
+    return chosenClass, alive, gold + addedGold
 
 def main():
     lvl = 1
     alive = True
     time = 0
-    gold = 100
+    gold = 100000
     potions = ["Health Potion"]
     
     startInput()
@@ -422,15 +568,15 @@ def main():
     chosenClass = chooseClass(name)
     maxHp = chosenClass.hp
     currentWeapon = Item("Rusty Knife", 1)
-    weapons = [currentWeapon]
+    weapons = [currentWeapon, Item("HEj", 2)]
     
     while alive:
         answer, time = chooseAction(time)
         if answer == "1":
             odds = continueAdventure()
-            if odds < 30:
+            if odds < 0:
                 chosenClass, lvl, gold, alive = monsterFight(chosenClass, lvl, gold,currentWeapon)
-            elif odds >= 30 and odds < 50:
+            elif odds >= 0 and odds < 0:
                 itemType, recievedItem = openChest(chosenClass)
                 if itemType == "Gold":
                     gold += recievedItem
@@ -440,18 +586,22 @@ def main():
                     weapons.append(recievedItem)
                     if recievedItem.strength > currentWeapon.strength:
                         currentWeapon = recievedItem
-            elif odds >= 50 and odds < 75:
-                recievedItem, gold = merchant(gold, chosenClass)
+            elif odds >= 1 and odds < 100:
+                recievedItem, weapons, gold = merchant(gold, chosenClass, weapons, potions)
                 if recievedItem == "Health Potion":
                     potions.append(recievedItem)
                 elif recievedItem != "" and recievedItem.strength > currentWeapon.strength:
                     currentWeapon = recievedItem
-                    weapons.append(recievedItem)
-                elif recievedItem != "":
-                    weapons.append(recievedItem)
+                elif currentWeapon not in weapons:
+                    currentWeapon = weapons[0]
+                    for i in range(0, len(weapons)):
+                        if currentWeapon.strength < weapons[i].strength:
+                            currentWeapon = weapons[i]
+
+
                 
-            elif odds >= 75:
-                chosenClass, alive = fallInTrap(chosenClass, alive)
+            elif odds >= 100:
+                chosenClass, alive, gold = fallInTrap(chosenClass, alive, gold, maxHp)
         elif answer == "2":
             chosenClass, potions = checkInventory(weapons, gold, potions, chosenClass, maxHp)
         elif answer == "3":
